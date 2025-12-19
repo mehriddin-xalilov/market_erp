05c910bdfda4bb4f97618f2a5f550e54af39b848
oks <xalilov.mehri@gmail.com>
2025-12-19 17:22:32 +0500

    feat: add custom brand logo to Filament admin panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,6 +29,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->brandLogo(asset('logos/logo.svg'))
             ->colors([
                 'primary' => Color::Amber,
             ])
